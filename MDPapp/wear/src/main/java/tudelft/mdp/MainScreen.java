ea6d88b5b6cefe85f7cdf44172d59aea067e958b
t7reyeslua <t7.reyeslua@gmail.com>
2014-09-16 16:49:47 +0200

    Initial code commit
    Google Login example

---
+++ MDPapp/wear/src/main/java/tudelft/mdp/MainScreen.java
@@ -0,0 +1,24 @@
+package tudelft.mdp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainScreen extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
