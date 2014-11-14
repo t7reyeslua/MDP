4f945dc298d1767e831875a2bf2998a477daacd1
Luis Gonzalez <l.a.GonzalezGodinez@student.tudelft.nl>
2014-11-14 16:17:23 +0100

    (Java/Online) Instances fix ,GetLocMotARFFRealExp
    CreateInstanceSet now can read "?" feature values
    GetLocMotARFFRealExpto convert files from experiment dataset to arff

--- Sandbox/EclipseJava/WekaTesting/src/wekatesting/WekaDemo.java
+++ Sandbox/EclipseJava/WekaTesting/src/wekatesting/WekaDemo.java
@@ -19,12 +19,17 @@ public class WekaDemo {
 	
 	public static void main(String[] args) throws Exception {
 		
-//		DatasetQuickWekatest();
-		DatasetQuickLocationWekatest();
+		
+		
+		
+		DatasetQuickWekatest();
+//		DatasetQuickLocationWekatest();
 //		EvalSensorWindowSize();
 //		String pathmodel="C:\\Users\\LG\\Desktop\\watchSession";
 //		String pathmodel="C:\\Users\\LG\\Dropbox\\MDP-Neo\\RUN_3_NOV";
-//		String arfffileName=pathmodel+"\\ResMotLoc";
+//		String pathmodel="C:\\Users\\LG\\Desktop\\DatasetLG";
+//		String arfffileName=pathmodel+"\\ResMotLocTOTAL";
+//		GetLocMotARFFRealExp(pathmodel,arfffileName,false);
 //		GetLocMotARFF(pathmodel,arfffileName,300,true);
 				
 		
@@ -46,7 +51,102 @@ public class WekaDemo {
 		System.out.println("Program Done!");
 		
 	}
+	/**
+	 * @author Luis Gonzalez
+	 * @throws Exception 
+	 * @brief creates arff test for the dataset created on the Real Simulation
+	 * 	 */
+	private static void GetLocMotARFFRealExp(String folderpath,String ResultsFileName,boolean nametimestamp) {
+		System.out.println("Getting ARRRRRFF! EXP");
+		ArrayList<String> MotionNames = new ArrayList<String> ();
+		ArrayList<String> MotionValues = new ArrayList<String> ();
+		ArrayList<String> MotionAttributes = new ArrayList<String> ();
+		
+		ArrayList<String> LocationNames = new ArrayList<String> ();
+		ArrayList<String> LocationValues = new ArrayList<String> ();
+		ArrayList<String> LocationAttributes = new ArrayList<String> ();
 
+		ArrayList<String> ClassNames = new ArrayList<String> ();
+
+		
+		//TODO work with one folder
+		//Files need to be in separate folders "Location" & "Motion"
+		
+		getftofLocationlogExp(folderpath,LocationNames,LocationValues,LocationAttributes);
+		getftofMotionlogExp(folderpath,MotionNames,MotionValues,MotionAttributes); 
+		
+		ClassNames=GetNameClass(LocationNames);
+		
+		System.out.println("\n\n Begin Merging \n");
+		//**Writting part**//
+		if(nametimestamp==true)
+			ResultsFileName=ResultsFileName+"_"+String.valueOf(System.currentTimeMillis());
+		ResultsFileName=ResultsFileName+".arff";
+//		ResultsFileName=ResultsFileName+".arff";
+    	
+    	Writer writer = null;
+
+    	try {
+    	    writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(ResultsFileName), "utf-8"));
+    	    
+    	    writer.write("@relation Events"+"\n\n");  
+    	    
+    	    //first Location then Motion Attributes
+            for(int i=0; i<LocationAttributes.size(); i++)
+            	writer.write(LocationAttributes.get(i)+"\n");
+            
+            for(int i=0; i<MotionAttributes.size(); i++)
+            	writer.write(MotionAttributes.get(i)+"\n");
+                        	
+            writer.write("@attribute activity {");
+            for(int a=0; a<(ClassNames.size()-1); a++){
+            	//TODO Change names to start with a letter
+            	writer.write("A"+NameDecomposition(ClassNames.get(a))+",");
+            }
+        	//TODO Change names to start with a letter
+        	writer.write("A"+NameDecomposition(LocationNames.get(LocationNames.size()-1))+"}");
+            writer.write("\n\n");
+            writer.write("@data\n");
+            
+            
+            //Writting values
+            for(int m=0; m<LocationValues.size(); m++){
+            	//first location
+            	writer.write(LocationValues.get(m));
+        		writer.write(",");
+            	
+        		
+            	//searching for its Motion counterpart (if any)
+            	int fFlag=0;
+            	for(int n=0; n<MotionValues.size(); n++){
+            		
+
+            		if(LocationNames.get(m).equals(MotionNames.get(n))){
+//            			System.out.println(LocationNames.get(m)+" equals "+MotionNames.get(n)+"for m= "+m+" n= "+n);
+            			writer.write(MotionValues.get(n)+",");
+            			fFlag=1;
+            		}	
+            	}
+            	if(fFlag==0){//No location found, fill with inf.
+            		System.out.println(LocationNames.get(m)+" has no equal "+"for m= "+m);
+            		for(int z=0;z<126;z++){
+            			
+            			writer.write("?,");
+            		}
+            	}
+                //Writting Class
+            	//TODO Change names to start with a letter
+        		writer.write("A"+NameDecomposition(LocationNames.get(m))+"\n");
+            }
+    	    
+    	} catch (IOException ex) {
+    	  // report
+    	} finally {
+    	   try {writer.close();} catch (Exception ex) {}
+    	}
+    	
+    }
+	
 	/**
 	 * @author Luis Gonzalez
 	 * @throws Exception 
@@ -247,75 +347,260 @@ public class WekaDemo {
     	
     }
 	
-		private static void GetMotARFF(String folderpath,String ResultsFileName,int numofScans) {
-		System.out.println("Getting Motion ARRRRFF!");
-		ArrayList<String> MotionNames = new ArrayList<String> ();
-		ArrayList<String> MotionValues = new ArrayList<String> ();
-		ArrayList<String> MotionAttributes = new ArrayList<String> ();
-		
-		ArrayList<String> LocationNames = new ArrayList<String> ();
-		ArrayList<String> LocationValues = new ArrayList<String> ();
-		ArrayList<String> LocationAttributes = new ArrayList<String> ();
+	private static void GetMotARFF(String folderpath,String ResultsFileName,int numofScans) {
+	System.out.println("Getting Motion ARRRRFF!");
+	ArrayList<String> MotionNames = new ArrayList<String> ();
+	ArrayList<String> MotionValues = new ArrayList<String> ();
+	ArrayList<String> MotionAttributes = new ArrayList<String> ();
+	
+	ArrayList<String> LocationNames = new ArrayList<String> ();
+	ArrayList<String> LocationValues = new ArrayList<String> ();
+	ArrayList<String> LocationAttributes = new ArrayList<String> ();
 
-		ArrayList<String> ClassNames = new ArrayList<String> ();
+	ArrayList<String> ClassNames = new ArrayList<String> ();
 
-		
-		//TODO work with one folder
-		//Files need to be in separate folders "Location" & "Motion"
-		
-		getftofLocationlog(folderpath,LocationNames,LocationValues,LocationAttributes);
-		getftofMotionlog(folderpath,MotionNames,MotionValues,MotionAttributes,numofScans);
-		
-		ClassNames=GetNameClass(LocationNames);
-		
-		System.out.println("\n\n Begin Merging \n");
-		//**Writting part**//
-		ResultsFileName=ResultsFileName+"_Motion_"+String.valueOf(System.currentTimeMillis())+".arff";
-		ResultsFileName=ResultsFileName+".arff";
-    	
-    	Writer writer = null;
+	
+	//TODO work with one folder
+	//Files need to be in separate folders "Location" & "Motion"
+	
+	getftofLocationlog(folderpath,LocationNames,LocationValues,LocationAttributes);
+	getftofMotionlog(folderpath,MotionNames,MotionValues,MotionAttributes,numofScans);
+	
+	ClassNames=GetNameClass(LocationNames);
+	
+	System.out.println("\n\n Begin Merging \n");
+	//**Writting part**//
+	ResultsFileName=ResultsFileName+"_Motion_"+String.valueOf(System.currentTimeMillis())+".arff";
+	ResultsFileName=ResultsFileName+".arff";
+	
+	Writer writer = null;
 
-    	try {
-    	    writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(ResultsFileName), "utf-8"));
-    	    
-    	    writer.write("@relation Events"+"\n\n");  
-    	    
-    	    //Motion Attributes
-            
-            for(int i=0; i<MotionAttributes.size(); i++)
-            	writer.write(MotionAttributes.get(i)+"\n");
-                        	
-            writer.write("@attribute activity {");
-            for(int a=0; a<(ClassNames.size()-1); a++){
-            	//TODO Change names to start with a letter
-            	writer.write("A"+NameDecomposition(ClassNames.get(a))+",");
-            }
+	try {
+	    writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(ResultsFileName), "utf-8"));
+	    
+	    writer.write("@relation Events"+"\n\n");  
+	    
+	    //Motion Attributes
+        
+        for(int i=0; i<MotionAttributes.size(); i++)
+        	writer.write(MotionAttributes.get(i)+"\n");
+                    	
+        writer.write("@attribute activity {");
+        for(int a=0; a<(ClassNames.size()-1); a++){
         	//TODO Change names to start with a letter
-        	writer.write("A"+NameDecomposition(LocationNames.get(LocationNames.size()-1))+"}");
-            writer.write("\n\n");
-            writer.write("@data\n");
-            
-            
-            //Writting values
-            for(int m=0; m<MotionValues.size(); m++){
-            	//just Motion
-            	writer.write(MotionValues.get(m));
-        		writer.write(",");
-            	
+        	writer.write("A"+NameDecomposition(ClassNames.get(a))+",");
+        }
+    	//TODO Change names to start with a letter
+    	writer.write("A"+NameDecomposition(LocationNames.get(LocationNames.size()-1))+"}");
+        writer.write("\n\n");
+        writer.write("@data\n");
+        
+        
+        //Writting values
+        for(int m=0; m<MotionValues.size(); m++){
+        	//just Motion
+        	writer.write(MotionValues.get(m));
+    		writer.write(",");
+        	
 
-                //Writting Class
-            	//TODO Change names to start with a letter
-        		writer.write("A"+NameDecomposition(LocationNames.get(m))+"\n");
-            }
-    	    
-    	} catch (IOException ex) {
-    	  // report
-    	} finally {
-    	   try {writer.close();} catch (Exception ex) {}
-    	}
-    	
-    }
+            //Writting Class
+        	//TODO Change names to start with a letter
+    		writer.write("A"+NameDecomposition(LocationNames.get(m))+"\n");
+        }
+	    
+	} catch (IOException ex) {
+	  // report
+	} finally {
+	   try {writer.close();} catch (Exception ex) {}
+	}
+	
+}
+	
+	private static void getftofLocationlogExp(String Mainfolderpath,ArrayList<String> ftNames,ArrayList<String> ftValues,ArrayList<String> ftAttributes) {
+		System.out.println("Start ft Location Log");
+		String folderpath = Mainfolderpath+"\\Location\\";
+		String filepath;
+		ArrayList<String> FileNames = new ArrayList<String> ();
+		ArrayList<String> FullFileNames = new ArrayList<String> ();
+		ArrayList<String> NetworkNames = new ArrayList<String> ();
+		ArrayList<String> AttributeNetworks = new ArrayList<String> ();
+		ArrayList<String> ftValuesLocation = new ArrayList<String> ();
+		
+		
+		FileNames=Reader_From_Files.getFileNames(folderpath);
+		Collections.sort(FileNames);
+		
+		/*getting "Master list" of networks*/
+		for(int p=0;p<FileNames.size();p++){
+			filepath=folderpath+FileNames.get(p);
+						
+			Reader_From_Files  readFile = new Reader_From_Files(filepath);
+			ArrayList<String> NetworkStringList = new ArrayList<String>();
 
+			NetworkStringList=readFile.getFileContent();
+			String[] NetArray = NetworkStringList.get(0).split(",");
+			ArrayList<String> newNetworkNames = new ArrayList<String> ();
+						
+//				System.out.println("\nFile "+(p+1)+" has "+NetArray.length+" Nets");
+
+
+			for(int n=0;n<NetArray.length;n++){
+				newNetworkNames.add(NetArray[n]);
+//					System.out.println(newNetworkNames.get(n));
+			}
+			
+			for(int m=0;m<NetworkNames.size();m++){
+
+				for(int n=0;n<newNetworkNames.size();n++){
+					if(NetworkNames.get(m).equals(newNetworkNames.get(n))){
+//							System.out.println("removing  "+NetworkNames.get(m)+" "+newNetworkNames.get(n) +" from "+m+" & "+n);
+						newNetworkNames.remove(NetworkNames.get(m));
+					}
+				}
+			}
+	
+			NetworkNames.addAll(newNetworkNames);
+						
+//				System.out.println("Total merged  are  "+NetworkNames.size());
+//				for(int n=0;n<NetworkNames.size();n++)
+//					System.out.println(NetworkNames.get(n));			
+		}
+		
+		
+		
+		/*******builing Array of values with "Master list" of networks**/
+		for(int p=0;p<FileNames.size();p++){
+			System.out.println(p+ " File "+FileNames.get(p));
+			filepath=folderpath+"\\"+FileNames.get(p);
+			
+			
+			Reader_From_Files  readFile = new Reader_From_Files(filepath);
+			ArrayList<String> NetworkStringList = new ArrayList<String>();
+			String ftString="";
+
+			NetworkStringList=readFile.getFileContent();
+			String[] NetArray = NetworkStringList.get(0).split(",");//Gets headers (Network names)
+			String[] NetValueArray = NetworkStringList.get(1).split(",");//get values
+			
+//				ArrayList<String> tempNetworkNames = new ArrayList<String> ();
+//				ArrayList<String> tempNetworkValuesNames = new ArrayList<String> ();
+//				
+//				for(int n=0;n<NetArray.length;n++){
+//					tempNetworkNames.add(NetArray[n]);				
+//					tempNetworkValuesNames.add(NetValueArray[n]);	
+//					
+//				}
+			
+						for(int m=0;m<NetworkNames.size();m++){
+				int Nflag=0;
+				for(int n=0;n<NetArray.length;n++){
+					
+					if(NetworkNames.get(m).equals(NetArray[n])){
+						double value = Double.parseDouble(NetValueArray[n]);
+						ftString=ftString+String.format("%.2f",value)+",";
+//							ftString=ftString+NetValueArray[n]+",";
+						Nflag=1;
+					}
+
+				}	
+				if(Nflag==0)
+					
+					ftString=ftString+"?"+",";
+			}
+					
+			
+			String[] fileNameparts = FileNames.get(p).split("_");
+//			String[] roundtime = fileNameparts[5].split(".txt");
+//			int roudnum =Integer.parseInt(roundtime[0]);		
+//			roudnum=roudnum/10;
+			FullFileNames.add(fileNameparts[2]+"_"+fileNameparts[3]);
+//				System.out.println(ftString);
+			
+
+			ftValuesLocation.add(deletelastchar(ftString,','));
+			
+						
+		}		
+		
+		//NetworkNames carry on the attributes list
+		//ftValuesLocation has its values
+		
+		//******Creating Attributes array*******//
+		
+		
+		for(int m=0;m<NetworkNames.size();m++){
+			//TODO Change Networknames to start with a letter
+			AttributeNetworks.add("@attribute N"+NetworkNames.get(m)+" numeric");
+		}
+		
+//			Reader_From_Files.write2File(AttributeNetworks,"ftLocNets");
+		
+
+		ftNames.addAll(FullFileNames);		
+		ftValues.addAll(ftValuesLocation);
+		ftAttributes.addAll(AttributeNetworks);		
+	}
+
+	private static void getftofMotionlogExp(String Mainfolderpath,ArrayList<String> ftNames,ArrayList<String> ftValues,ArrayList<String> ftAttributes){
+			System.out.println("Start readlog()");
+			String folderpath = Mainfolderpath+"\\Motion\\";
+			String filepath;
+			ArrayList<String> FileNames = new ArrayList<String> ();
+			ArrayList<String> ftMotionArray = new ArrayList<String> ();
+			ArrayList<String> fullFileNames = new ArrayList<String> ();
+			
+			FileNames=Reader_From_Files.getFileNames(folderpath);
+			Collections.sort(FileNames);
+			
+			for(int p=0;p<FileNames.size();p++){
+				System.out.println(p+ " File "+FileNames.get(p));
+				filepath=folderpath+"\\"+FileNames.get(p);
+				
+				
+				Reader_From_Files  readFile = new Reader_From_Files(filepath);
+				ArrayList<String> SensorStringList = new ArrayList<String>();
+			
+				HashMap <String, ArrayList<Double>> SensorsArrays;
+				String ftString;
+
+				SensorStringList = readFile.getFileContent();
+				
+//				SensorStringList = readFile.Sensors2arraylists();
+				
+//				WekaSensorsRawDataObject SensorRaw =new WekaSensorsRawDataObject();
+//				
+//				SensorRaw.setSensorReadings(SensorStringList);
+//				SensorRaw.buildSensorArrays();
+//				SensorsArrays = new HashMap<String, ArrayList<Double>>(SensorRaw.getSensorsArrays());
+//
+//				
+
+//				ftString=SensorRaw.getFeatures(numofScans);
+				
+				ftString=SensorStringList.get(0);
+				String[] fileNameparts = FileNames.get(p).split("_");
+//				String[] roundtime = fileNameparts[5].split(".txt");
+//				int roudnum =Integer.parseInt(roundtime[0]);		
+//				roudnum=roudnum/10;
+				fullFileNames.add(fileNameparts[2]+"_"+fileNameparts[3]);
+				ftMotionArray.add(ftString);
+				
+//				System.out.println(ftString);
+
+				
+			}
+			
+//			Reader_From_Files.write2File(ftMotionArray,"ftMotNoNames");
+			
+			WekaSensorsRawDataObject SensorAt =new WekaSensorsRawDataObject();
+			
+			
+			ftNames.addAll(fullFileNames);
+			ftAttributes.addAll(SensorAt.getAttributes());
+			ftValues.addAll(ftMotionArray);
+			
+		}
+			
 	private static void getftofLocationlog(String Mainfolderpath,ArrayList<String> ftNames,ArrayList<String> ftValues,ArrayList<String> ftAttributes) {
 		System.out.println("Start ft Location Log");
 		String folderpath = Mainfolderpath+"\\Location\\";
